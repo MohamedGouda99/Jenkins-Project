3a395684b334c365f3a6f08b2665a9f087decf13
MohamedGouda99 <ma1582830@gmail.com>
2023-01-13 18:34:32 +0200

    ..

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ def buildImage(){
 def incrementVersion(){
     echo "incrementing app version..."
     sh "mvn build-helper:parse-version versions:set \
-        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.MinorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit"
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit"
 
         def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
         def version = matcher[0][1]
